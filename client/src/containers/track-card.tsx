5a087d034fc372138af4ccab15ef1c147c919202
Andrii Gadar <gadar.andre@gmail.com>
2024-05-31 09:19:13 +0300

    step7

--- client/src/containers/track-card.tsx
+++ client/src/containers/track-card.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "@emotion/styled";
 import { colors, mq } from "../styles";
 import { humanReadableTimeFromSeconds } from "../utils/helpers";
@@ -6,10 +7,10 @@ import type { Track } from '../__generated__/graphql'
 
 
 const TrackCard: React.FC<{ track: Track }> = ({ track }) => {
-  const { title, thumbnail, author, length, modulesCount } = track;
+  const { title, thumbnail, author, length, modulesCount, id } = track;
 
   return (
-    <CardContainer>
+    <CardContainer to={`/track/${id}`}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail || ""} alt={title} />
@@ -35,7 +36,7 @@ const TrackCard: React.FC<{ track: Track }> = ({ track }) => {
 export default TrackCard;
 
 /** Track Card styled components */
-const CardContainer = styled.div({
+const CardContainer = styled(Link)({
   borderRadius: 6,
   color: colors.text,
   backgroundSize: "cover",
